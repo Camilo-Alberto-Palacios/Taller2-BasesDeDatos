113c178a87f2ec076d035639338f659595162121
Camilo Alberto Palacios Turriago <alberto.palacios@est.iudigital.edu.co>
2025-09-14 09:57:51 -0500

    Update 00_create_database.sql
    crea la base de datos proyectos_informaticos

--- sql/00_create_database.sql
+++ sql/00_create_database.sql
@@ -1,2 +1,7 @@
-CREATE DATABASE IF NOT EXISTS proyectos_informaticos;
-USE proyectos_informaticos;
+--==============================================================================
+--                      Archivo: 00_create_database.sql
+--                  Descripcion: crea la base de datos proyectos_informaticos, 
+--                               si existe omite y toma por defecto la tabla
+--============================================================================== 
+CREATE DATABASE IF NOT EXISTS proyectos_informaticos;  -- se crea la base de datos proyecto informaticos, si ya esta creada lo omite
+USE proyectos_informaticos;                            -- le inidica al editor que use esta base de datos
